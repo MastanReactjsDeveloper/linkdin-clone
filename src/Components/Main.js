cf0aeaabfaabec00d32a40b704820c0051e9bed3
Mastan <komodheen.mastan@krazybee.com>
2023-08-15 18:10:47 +0530

    added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,41 +3,59 @@ import styled from "styled-components"
 
 const Main = (props) => {
     return <Container>
+
         <ShareBox>
-        <div>
-            <img src="/images/user.svg" alt="" />
-            <button>Share  a post</button>
-        </div>
-        <div>
-            <button>
-                <svg style={{ color: "blue" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
-                    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                    <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z" />
-                </svg>
-                <span>Photo</span>
-            </button>
-            <button >
-                <svg style={{ color: "green" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16">
-                    <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-                </svg>
-                <span>Vidoes</span>
-            </button>
-            <button>
-                <svg style={{ color: "red" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-calendar2-event-fill" viewBox="0 0 16 16">
-                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zm9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5zM11.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
-                </svg>
-                <span>Events</span>
-            </button>
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                </svg>
-                <span>Write article</span>
-            </button>
-        </div>
+
+            <div>
+                <img src="/images/user.svg" alt="" />
+                <button>Start a post</button>
+            </div>
+            <div>
+                <button>
+                    <svg style={{ color: "blue" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
+                        <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                        <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z" />
+                    </svg>
+                    <span>Photo</span>
+                </button>
+                <button >
+                    <svg style={{ color: "green" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play-btn-fill" viewBox="0 0 16 16">
+                        <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+                    </svg>
+                    <span>Vidoes</span>
+                </button>
+                <button>
+                    <svg style={{ color: "red" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-calendar2-event-fill" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zm9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5zM11.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+                    </svg>
+                    <span>Events</span>
+                </button>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                    </svg>
+                    <span>Write article</span>
+                </button>
+            </div>
         </ShareBox>
-       
+        <Article>
+            <SharedActor>
+                <a >
+                    <img src="/images/user.svg" alt="" />
+                    <div>
+                        <span>title</span>
+                        <span>info</span>
+                        <span>Date</span>
+                    </div>
+                </a>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                    </svg>
+                </button>
+            </SharedActor>
+        </Article>
     </Container>;
 };
 
@@ -88,11 +106,80 @@ const ShareBox = styled(CommonCard)`
         flex-grow:1;
         border-radius:35px;
         padding-left:16px;
-        border:1px solid rgba(0,0,0,0.15)
+        border:1px solid rgba(0,0,0,0.15);
+        background-color:white;
+        text-align:left;
     }
     }
-   
+   &:nth-child(2){
+     display: flex;
+     flex-wrap:wrap;
+     justify-content:space-around;
+     padding-bottom:4px;
+     button{
+        img{
+            margin:0 4px 0 -2px;
+        }
+        span{
+            color:#70b5f9;
+            padding:5px;
+        }
+     }
+   }
   }
   `;
+const Article = styled(CommonCard)`
+  padding:0;
+  margin:0 0 8px;
+  overflow: visible;
+  `;
 
+const SharedActor = styled.div`
+padding:40px;
+flex-wrap:nowrap;
+padding:12px 16px 0px;
+margin-bottom:8px;
+align-items:center;
+display:flex;
+a{
+    margin-right:12px;
+    flex-grow:1;
+    overflow:hidden;
+    display: flex;
+    text-decoration:none;
+
+    img{
+        width:48px;
+        height:48px;
+    }
+    a > div{
+        display:flex;
+        flex-direction:column;
+        flex-grow:1;
+        flex-basis:0;
+        margin-left:8px;
+        overflow:hidden;
+        span{
+            text-align:left;
+            &:first-child{
+                font-size:14px;
+                font-weight:700;
+                color:rgba(0,0,0,1);
+            }
+            &:nth-child(n+1){
+                font-size:12px;
+                color:rgba(0,0,0,0.6);
+            }
+        }
+    }
+}
+button{
+    position:absolute;
+    right:12px;
+    top:0px;
+    background:transparent;
+    border:none;
+    outline:none;
+}
+`;
 export default Main
